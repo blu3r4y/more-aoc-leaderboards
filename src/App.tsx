22521420ec2720268ad89af07bc3d584e075d732
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2021-12-30 12:46:21 +0100

    Adapt app to desired style

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={process.env.PUBLIC_URL + "/logo.svg"} className="App-logo" alt="logo" />
+        <p>More AoC Leaderboards!</p>
       </header>
     </div>
   );
