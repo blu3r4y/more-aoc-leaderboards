5cd028c308bb951d01eb8a77826e3a1f7222eb89
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2022-03-07 21:35:11 +0100

    Encode payload version directly into the URL fragment

--- src/api/ApiEncoder.ts
+++ src/api/ApiEncoder.ts
@@ -4,19 +4,25 @@ import { IApiData, Schema } from "./ApiTypes";
 import { compress, decompress } from "../utils/LzmaUtils";
 
 export async function encodeData(apiData: IApiData): Promise<string> {
-  const pack = JSON.stringify({ version: 1, payload: apiData });
+  const version = 1;
+
+  const pack = JSON.stringify({ payload: apiData });
   const buffer = await compress(pack);
   const base64 = Buffer.from(buffer).toString("base64");
   const code = base64.replaceAll("+", "-").replaceAll("/", "_").replaceAll("=", "%3d");
-  return code;
+  const hash = `${version}~${code}`;
+  return hash;
 }
 
-export async function decodeData(code: string): Promise<IApiData> {
+export async function decodeData(hash: string): Promise<IApiData> {
+  const version = parseInt(hash.substring(0, 1));
+  console.assert(version === 1, `invalid hash version: ${version}`);
+
+  const code = hash.substring(2);
   const base64 = code.replaceAll("-", "+").replaceAll("_", "/").replaceAll("%3d", "=");
   const buffer = Buffer.from(base64, "base64");
   const pack = await decompress(buffer);
   const data = JSON.parse(pack) as { version: number; payload: IApiData };
-  console.assert(data.version === 1, `invalid data version: ${data.version}`);
   await Schema.validateAsync(data.payload);
   return data.payload;
 }
